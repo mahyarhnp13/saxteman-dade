a2f26e3d15d0282f2e58c7ff3062a75890c07b81
mahyarhnp13 <129826878+mahyarhnp13@users.noreply.github.com>
2023-11-02 23:11:54 +0330

    Update tawan.py
    بدون ورودی
    
    Signed-off-by: mahyarhnp13 <129826878+mahyarhnp13@users.noreply.github.com>

--- tawan.py
+++ tawan.py
@@ -3,8 +3,6 @@ def twn(x, y):
         return 1
     else:
         return x * twn(x, y-1)
-x = int(input("Enter number one: "))
-y = int(input("Enter number two: "))
-
+        
 result = twn(x, y)
 print(result)
