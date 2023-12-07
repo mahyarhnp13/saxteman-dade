69128af8a6c2dec290bf6e8554e808b34f5a19f1
mahyar13 <mahyarhoseinpour13@gmail.com>
2023-12-08 01:24:20 +0330

    new function added

--- New Text Document.py
+++ New Text Document.py
@@ -1,3 +1,10 @@
 class MyArray:
     def __init__(self):
         self.array = []
+    def insert(self, obj, index):
+        self.array.insert(index, obj)
+    def delete(self, index):
+        if 0 <= index < len(self.array):
+            return self.array.pop(index)
+        else:
+            print("خطا: ایندکس معتبر نیست.")
